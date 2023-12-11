29ed55a7e6be7a103c3e751f28f63f14f89f89fc
Rahul Reddy <iamrahul250@gmail.com>
2023-12-11 11:07:47 +0530

    Added nav menu and hyperlinks

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-var menu = document.getElementById('menu');
-        var menuBtn = document.getElementById('menuBtn');
-        var closeBtn = document.getElementById('closeBtn');
-
-        menuBtn.addEventListener('click', function() {
-            toggleMenu();
-        });
-
-        closeBtn.addEventListener('click', function() {
-            toggleMenu();
-        });
-
-        document.addEventListener('click', function(e) {
-            if (!menu.contains(e.target) && e.target !== menuBtn) {
-                if (menu.classList.contains('active')) {
-                    toggleMenu();
-                }
-            }
-        });
-
-        function toggleMenu() {
-            menu.classList.toggle('active');
-
-            if (menu.classList.contains('active')) {
-                gsap.from('.menu a', {opacity: 0, y: -50, stagger: 0.2, delay: 0.2});
-            }
-        }
+document.getElementById('menu-toggle').addEventListener('click', function()
+		{
+			var menu = document.getElementById('mobile-menu');
+			menu.classList.toggle('hidden');
+		});
+		// Function to check if the menu is open
+		function isMenuOpen()
+		{
+			return !document.getElementById('mobile-menu').classList.contains('hidden');
+		}
+		// Add a click event listener to the document to close the menu when clicking outside
+		document.addEventListener('click', function(event)
+		{
+			if (!event.target.closest('#menu-toggle') && isMenuOpen())
+			{
+				document.getElementById('mobile-menu').classList.add('hidden');
+			}
+		});
+		// Close the menu when an option is selected
+		var menuOptions = document.querySelectorAll('#mobile-menu a');
+		menuOptions.forEach(function(menuOption)
+		{
+			menuOption.addEventListener('click', function()
+			{
+				document.getElementById('mobile-menu').classList.add('hidden');
+			});
+		});
+		// Add an event listener to close the menu on resize (for responsiveness)
+		window.addEventListener('resize', function()
+		{
+			if (window.innerWidth >= 768)
+			{ // Adjust the breakpoint as needed
+				document.getElementById('mobile-menu').classList.add('hidden');
+			}
+		});
